0656ed7d25aef3f2b0b475830f5d07da5e92e4b2
matteokeole <matteoo34@icloud.com>
2022-12-11 23:09:47 +0100

    Notif bar closing keyframe and tests with Eeeude :D

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,30 @@
 function closeNotification() {
 	const bar = document.querySelector("#notif-bar");
-	bar.remove();
+
+	bar.classList.add("closed");
+	setTimeout(function() {
+		bar.remove();
+	}, 500);
 }
 
 // camelCase
 // PascalCase
 // snake_case
 // SCREAMING_SNAKE_CASE
+
+/* let test = 2;
+let chaineDeCaracteres = "Ce que tu veux. ;)";
+let chaineDeCaracteres2 = 'Poutou';
+const tableau = [3, 8, 2, 5];
+const index = 1;
+console.log(tableau.length)
+let eude = {
+	firstName: "Eeeude",
+	lastName: chaineDeCaracteres2,
+	parents: "Camille & Caroline",
+	age: 15,
+	faireUneCriseDAdo: function() {
+		console.log(this.firstName + " fait une crise d'adooo !!!")
+	}
+}
+console.log(eude.faireUneCriseDAdo()) */
