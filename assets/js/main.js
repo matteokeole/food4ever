6c4c6da6d0cd581013c0e1a2cffdeb1d7e78b1d3
matteokeole <matteoo34@icloud.com>
2022-12-11 13:43:04 +0100

    Notification bar event listener

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log("Bonjour.")
+function closeNotification() {
+	const bar = document.querySelector("#notif-bar");
+	bar.remove();
+}
+
+// camelCase
+// PascalCase
+// snake_case
+// SCREAMING_SNAKE_CASE
